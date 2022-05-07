ae3cfd1082f5c941373919a9c6f0f4ed9bf1d96a
Jose Migel Gonzales Gutierrez <josemigel@gmail.com>
2022-05-06 20:43:00 -0600

    parametros jose

---
+++ app/src/main/java/com/example/parametros/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.parametros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+
+        String dato = getIntent().getStringExtra("dato");
+        tv1.setText("Hola " + dato);
+
+    }
+
+
+    //Metodo para el botón Regresar
+    public void Regresar(View view) {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+
+    }
+}
